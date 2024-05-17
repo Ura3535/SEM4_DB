18d0e39f29fe370695095c009504a9813a11408e
Yurii Kovalchuk <urakovalchuk3535@gmail.com>
2024-05-17 12:26:50 +0300

    FacilityType connect to rep

--- Lab2/FacilityTypeController.cpp
+++ Lab2/FacilityTypeController.cpp
@@ -1,9 +1,8 @@
 #include "Lab2Form.h"
 #include "Repository.h"
 
-using namespace System::Runtime::InteropServices;
 using namespace Lab2;
-using namespace Models;
+using namespace Repository;
 
 Void Lab2Form::FT_ButtonAdd_Click(Object^ sender, EventArgs^ e)
 {
@@ -11,9 +10,44 @@ Void Lab2Form::FT_ButtonAdd_Click(Object^ sender, EventArgs^ e)
     {
         FacilityType^ tmp = gcnew FacilityType();
         tmp->Type = FT_TextBoxAddType->Text;
+        rep->Add(Table::FacilityTypes, tmp);
     }
-    catch (const std::exception&) {}
-    finally {
-        Marshal::FreeHGlobal(type_ptr);
+    catch (...) {}
+}
+
+Void Lab2Form::FT_ButtonUPDELSave_Click(Object^ sender, EventArgs^ e)
+{
+    try
+    {
+        FacilityType^ tmp = gcnew FacilityType();
+        tmp->Id = Convert::ToInt64(FT_ComboBoxUPDELId->Text);
+        tmp->Type = FT_TextBoxUPDELType->Text;
+        rep->Update(Table::FacilityTypes, tmp);
     }
+    catch (...) {}
+}
+
+Void Lab2Form::FT_ButtonUPDELRollBack_Click(Object^ sender, EventArgs^ e)
+{
+    try
+    {
+        FacilityType^ tmp = (FacilityType^)rep->Get(Table::FacilityTypes, Convert::ToInt64(FT_ComboBoxUPDELId->Text));
+        tmp->Type = FT_TextBoxAddType->Text;
+        rep->Add(Table::FacilityTypes, tmp);
+    }
+    catch (...) {}
+}
+
+Void Lab2Form::FT_ButtonUPDELDelete_Click(Object^ sender, EventArgs^ e)
+{
+    try
+    {
+        rep->Delete(Table::FacilityTypes, Convert::ToInt64(FT_ComboBoxUPDELId->Text));
+    }
+    catch (...) {}
+}
+
+Void Lab2Form::FT_ComboBoxUPDELId_SelectedIndexChanged(Object^ sender, EventArgs^ e)
+{
+    FT_ButtonUPDELRollBack_Click(sender, e);
 }
